a63cf5b7669a0b5a894190b038512c5f89c15d3d
tofusupreme <yunishbacon@gmail.com>
2022-06-14 21:17:00 +0900

    Added text to Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,6 +4,12 @@ export default function Hero() {
   return (
     <div className="hero">
       <img src={groupImage} alt="groupImage" className="hero--image" />
+      <h1>
+        Online Experiences
+      </h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.
+      </p>
     </div>
   )
 }
