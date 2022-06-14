4588079fb9b1d54b5176e353b1ddeb0ebac36885
tofusupreme <yunishbacon@gmail.com>
2022-06-14 20:15:18 +0900

    Added airbnb img to navbar.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from './components/Navbar';
 import './style.css';
 function App() {
   return (
     <div className="container">
+      <Navbar />
       <h1> App Container </h1>
     </div>
   );
