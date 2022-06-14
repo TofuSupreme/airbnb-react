bc5f4848eebab96e8241d3f8b7b3968c9dc0f997
tofusupreme <yunishbacon@gmail.com>
2022-06-14 21:18:39 +0900

    added classNames to h1 and p tags in Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,10 +4,10 @@ export default function Hero() {
   return (
     <div className="hero">
       <img src={groupImage} alt="groupImage" className="hero--image" />
-      <h1>
+      <h1 className="hero--h1">
         Online Experiences
       </h1>
-      <p>
+      <p className="hero--p">
         Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.
       </p>
     </div>
