985d31755cb3236b465d67437c833fd7439d925c
tofusupreme <yunishbacon@gmail.com>
2022-06-14 21:01:32 +0900

    added Hero.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
 import './style.css';
 function App() {
   return (
     <div className="container">
       <Navbar />
+      <Hero />
     </div>
   );
 }
