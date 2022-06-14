513683ef1518d5b5c558f8a165d51842c139a7df
tofusupreme <yunishbacon@gmail.com>
2022-06-14 21:00:39 +0900

    Added group image to Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import groupImage from "../images/groupImage.png"
 export default function Hero() {
   return (
     <div className="hero">
+      <img src={groupImage} alt="groupImage" className="hero--image" />
     </div>
   )
 }
