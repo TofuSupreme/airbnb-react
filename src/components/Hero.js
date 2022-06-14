6ff945f349d44517b64abed37fd4264c43dc8a3a
tofusupreme <yunishbacon@gmail.com>
2022-06-14 21:45:53 +0900

    changed classNames on Hero.js as well as changed some styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,14 +2,14 @@ import React from "react";
 import groupImage from "../images/groupImage.png"
 export default function Hero() {
   return (
-    <div className="hero">
+    <section className="hero">
       <img src={groupImage} alt="groupImage" className="hero--image" />
-      <h1 className="hero--h1">
+      <h1 className="hero--header">
         Online Experiences
       </h1>
-      <p className="hero--p">
+      <p className="hero--text">
         Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.
       </p>
-    </div>
+    </section>
   )
 }
