88c5726a506e6a39d08b880c270d7a35677d0bba
tofusupreme <yunishbacon@gmail.com>
2022-06-16 18:54:12 +0900

    imported card component to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Card from './components/Card';
 
 import './style.css';
 function App() {
   return (
     <div className="container">
       <Navbar />
-      <Hero />
+      <Card />
+      {/* <Hero /> */}
     </div>
   );
 }
