d39b5b1aecf5e481ca85f1076f40ac45f17be2c2
tofusupreme <yunishbacon@gmail.com>
2022-06-21 21:59:43 +0900

    added style to star img

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,9 +5,9 @@ import zaferes from "../images/zaferes.png";
 export default function Card() {
   return (
     <section className="card">
-      <img src={zaferes} alt="zaferes" className="card--image" />
-      <p className="rating">
-      <img src={star} alt="star" className="card--image" />
+      <img src={zaferes} alt="zaferes" className="card--image--zaferes" />
+      <p className="card--rating">
+      <img src={star} alt="star" className="card--image--star" />
         5.0 (6) * USA <br></br>
         Life lessons with Katie Zaferes<br></br>
         <span className="card--price">From $136</span> / person
