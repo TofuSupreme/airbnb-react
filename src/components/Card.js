2399334a71333b8c9d989aef51173f459f796a14
tofusupreme <yunishbacon@gmail.com>
2022-06-23 16:26:42 +0900

    cleaned up styles to first div

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,19 +9,15 @@ export default function Card() {
       <img className ="card--image" src={zaferes} alt="zaferes" />
         <button className="card--button--status"> SOLD OUT</button>
         <div className="card--stats">
-        <img src={star} alt="star" className="card--star" />
-        <span> 5.0 </span>
-        <span className="gray"> (6) • </span>
-        <span className="gray"> USA </span>
+          <img src={star} alt="star" className="card--star" />
+          <span> 5.0 </span>
+          <span className="gray"> (6) • </span>
+          <span className="gray"> USA </span>
         </div>
-        <p className="card--rating--info">
-          Life lessons with Katie Zaferes
-      </p>
-      <p>
-        <span className="card--rating--price">From $136
-        </span>
-        / person
-      </p>
+        <p> Life lessons with Katie Zaferes
+        </p>
+        <p> <span className="bold">From $136</span>/ person
+        </p>
 
       {/* <img src={weddingPhoto} alt="weddingPhoto" className="card--image" />
       <button className="card--button--status"> SOLD OUT</button>
