e1bb392e277f944f23c611b201cd6aafa020d77c
tofusupreme <yunishbacon@gmail.com>
2022-07-21 19:28:26 +0900

    still expericing img bug

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import star from "../images/star.png";
 export default function Card(props) {
   return (
     <div className="card--container">
-      <img className ="card--image" {...props.img} alt="" />
+      <img className ="card--image" src={'src/images/' + props.img + '.png'} alt={props.img}/>
         {/* <button className="card--button--status"> SOLD OUT</button> */}
         <div className="card--stats">
           <img src={star} alt="star" className="card--star" />
