1b790fa96b11e5c52c23ee63bdd7cb4a6c5cca83
tofusupreme <yunishbacon@gmail.com>
2022-07-21 15:26:43 +0900

    added new file data.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
-import zaferes from './images/zaferes.png';
+// import zaferes from './images/zaferes.png';
+import airBnbData
+  from './components/data';
 import './style.css';
 function App() {
-  return (
-    <div className="container">
-      <Navbar />
-      <Hero />
-      <Card
-        img={zaferes}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136} />
-    </div>
-  );
-}
+  airBnbData.map(item => {
+    console.log(item);
+    return (
+      <div className="container">
+        <Navbar />
+        <Hero />
+        <Card
+          img={item.coverImg}
+          rating="5.0"
+          reviewCount={6}
+          country="USA"
+          title="Life Lessons with Katie Zaferes"
+          price={136} />
+
+      </div>
+    );
+  });
+};
 
 export default App;
