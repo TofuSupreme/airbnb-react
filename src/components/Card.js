8457e47da27999788a12aedc10b482e686f046e7
tofusupreme <yunishbacon@gmail.com>
2022-07-21 19:18:44 +0900

    removed pieces of bug from data.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import star from "../images/star.png";
 export default function Card(props) {
   return (
     <div className="card--container">
-      <img className ="card--image" src={props.img} alt="zaferes" />
+      <img className ="card--image" {...props.img} alt="" />
         {/* <button className="card--button--status"> SOLD OUT</button> */}
         <div className="card--stats">
           <img src={star} alt="star" className="card--star" />
@@ -17,22 +17,6 @@ export default function Card(props) {
         </p>
       <p> <span className="bold">From {props.price}</span>/ person
         </p>
-
-      {/* <img src={weddingPhoto} alt="weddingPhoto" className="card--image" />
-      <button className="card--button--status"> SOLD OUT</button>
-      <div className="card--stats">
-        <img src={star} alt="star" className="card--image--star" />
-        <span> 5.0 </span>
-        <span> (30) • </span>
-        <span> USA </span>
-
-        <p className="card--rating--info">
-          Learn wedding photography
-        </p>
-        <p>
-          <span className="card--rating--price"> From $125</span>
-        / person</p>
-      </div> */}
     </div>
 
   )
--- src/components/data.js
+++ src/components/data.js
@@ -1,14 +1,10 @@
-import zaferes from "../images/zaferes.png";
-import weddingPhoto from "../images/weddingPhoto.png";
-import mountainBike from "../images/mountainBike.png";
-
 const airBnbData = [
     {
         id: 1,
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: <img src={zaferes} alt="zaferes" className="card--image" />,
+        coverImg: "zaferes",
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -21,7 +17,7 @@ const airBnbData = [
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: <img src={weddingPhoto} alt="weddingPhoto" className="card--image" />,
+        coverImg: "weddingPhoto",
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -34,9 +30,7 @@ const airBnbData = [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-      coverImg: <img src={mountainBike}
-        alt="mountain-bike"
-      className="card--image"/>,
+        coverImg: "mountain-bike.png",
         stats: {
             rating: 4.8,
             reviewCount: 2
