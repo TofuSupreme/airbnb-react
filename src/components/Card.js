1526450f07a730f16edc06c4ea33c8d99ade540f
tofusupreme <yunishbacon@gmail.com>
2022-07-24 15:27:17 +0900

    cleaned up App.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,29 @@
 import React from "react";
 
 export default function Card(props) {
+  console.log(props)
+
   let badgeText
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = "SOLD OUT"
-  } else if (props.location === "Online") {
+  } else if (props.item.location === "Online") {
     badgeText = "ONLINE"
   }
 
   return (
     <div className="card">
       {badgeText && <div className="card--badge"> {badgeText} </div>}
-      <img src={`${process.env.PUBLIC_URL}/assets/images/${props.img}`}  className="card--image" alt={props.img}/>
+      <img src={`${process.env.PUBLIC_URL}/assets/images/${props.item.coverImg}`}  className="card--image" alt={props.item.coverImg}/>
         <div className="card--stats">
           <img src={`${process.env.PUBLIC_URL}/assets/images/star.png`} alt="star" className="card--star" />
-          <span> {props.rating} </span>
-          <span className="gray"> ({props.reviewCount}) • </span>
-          <span className="gray"> {props.country} </span>
+          <span> {props.item.stats.rating} </span>
+          <span className="gray"> ({props.item.stats.reviewCount}) • </span>
+          <span className="gray"> {props.item.location} </span>
         </div>
         <p className="card--title">
-        {props.title}
+        {props.item.title}
         </p>
-      <p className="card--price"> <span className="bold">From {props.price}</span>/ person
+      <p className="card--price"> <span className="bold">From {props.item.price}</span>/ person
         </p>
     </div>
 
