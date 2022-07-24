c6f4cf5558b3e13d1b8b4b5b588bc4b5e5eadc5c
tofusupreme <yunishbacon@gmail.com>
2022-07-24 14:20:08 +0900

    added card badge

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       country={item.location}
       title={item.title}
       price={item.price}
+      openSpots={item.openSpots}
     />
   });
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Card(props) {
   return (
     <div className="card">
+      <div className="card--badge"> SOLD OUT </div>
       <img src={`${process.env.PUBLIC_URL}/assets/images/${props.img}`}  className="card--image" alt={props.img}/>
-        {/* <button className="card--button--status"> SOLD OUT</button> */}
         <div className="card--stats">
           <img src={`${process.env.PUBLIC_URL}/assets/images/star.png`} alt="star" className="card--star" />
           <span> {props.rating} </span>
