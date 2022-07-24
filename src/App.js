5b77e7fc60c968bb309b970470a43b524b04e378
tofusupreme <yunishbacon@gmail.com>
2022-07-24 15:40:06 +0900

    added commments for passing props

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     console.log(item);
     return <Card
       key={item.id}
-      { ...item}
+      {...item} //can use {...item} instead of item.id, item.title, etc. and item={item} if you use item={item}, to access the object properties you would need to reference them as props.item.propertyName in the component. //
     />
   });
 
